b5118bd0fb23ab77240e307955547bfe549d13ed
Eli <elibailey97@gmail.com>
2020-06-05 14:23:36 -0400

    trying out CameraBoundsStuff

--- lib/screens/home/map_tab.dart
+++ lib/screens/home/map_tab.dart
@@ -10,6 +10,8 @@ import 'package:mvp/services/database.dart';
 import 'package:mvp/services/location.dart';
 import 'package:mvp/shared/loading.dart';
 import 'package:provider/provider.dart';
+import 'dart:math';
+import 'package:vector_math/vector_math.dart';
 
 class MapTab extends StatefulWidget {
   @override
@@ -18,14 +20,25 @@ class MapTab extends StatefulWidget {
 
 class _MapTabState extends State<MapTab> {
 
+  final double EARTHRADIUS = 6366198;
+
   GoogleMapController _mapsController;
 
   final LocationService _locationService = LocationService();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  CameraPosition _startPos;
+  CameraTargetBounds _cameraTargetBounds;
+
+  @override
+  void initState() {
+    super.initState();
+    _locationService.checkPermission();
+  }
+
+  void _setupCamera(UserData userData) {
+    _startPos = _createCameraPositionFromGP(userData.gp);
+    _cameraTargetBounds = _createMapBoundsFromGeoPoint(userData.gp, 50);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +49,12 @@ class _MapTabState extends State<MapTab> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             UserData userData = snapshot.data;
+
+            _setupCamera(userData);
+
             print(userData.firstName);
             print(userData.gp);
+
             return Container(
               height: MediaQuery
                   .of(context)
@@ -49,7 +66,8 @@ class _MapTabState extends State<MapTab> {
                   .width,
               child: GoogleMap(
                 mapType: MapType.normal,
-                initialCameraPosition: _createCameraPositionFromGP(userData.gp),
+                initialCameraPosition: _startPos,
+                cameraTargetBounds: _cameraTargetBounds,
                 myLocationEnabled: true,
                 onMapCreated: (GoogleMapController controller) {
                   setState(() {
@@ -70,5 +88,35 @@ class _MapTabState extends State<MapTab> {
       target: LatLng(gp.latitude, gp.longitude),
       zoom: 18.75
     );
+  }
+
+  CameraTargetBounds _createMapBoundsFromGeoPoint(GeoPoint gp, double boundsM) {
+    LatLng center = LatLng(gp.latitude, gp.longitude);
+    //calc Northeast point
+    LatLng northEast = _move(center, boundsM, boundsM);
+    //calc Southwest point
+    LatLng southWest = _move(center, -boundsM, -boundsM);
+    //make LatLngBounds and return CameraTargetBounds
+    return CameraTargetBounds(LatLngBounds(northeast: northEast, southwest: southWest));
+  }
+
+  LatLng _move(LatLng startLL, double toNorth, double toEast) {
+    double lonDiff = _meterToLongitude(toEast, startLL.latitude);
+    double latDiff = _meterToLatitude(toNorth);
+    return new LatLng(startLL.latitude + latDiff, startLL.longitude
+        + lonDiff);
+  }
+
+  double _meterToLongitude(double meterToEast, double latitude) {
+    double latArc = radians(latitude);
+    double radius = cos(latArc) * EARTHRADIUS;
+    double rad = meterToEast / radius;
+    return degrees(rad);
+  }
+
+
+  double _meterToLatitude(double meterToNorth) {
+    double rad = meterToNorth / EARTHRADIUS;
+    return degrees(rad);
   }
 }
