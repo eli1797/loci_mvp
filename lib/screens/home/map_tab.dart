596221362743191ea93f15b5bda8ac92c47ed287
Eli <elibailey97@gmail.com>
2020-06-04 21:06:06 -0400

    basic map working!

--- lib/screens/home/map_tab.dart
+++ lib/screens/home/map_tab.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapTab extends StatefulWidget {
   @override
@@ -6,10 +9,24 @@ class MapTab extends StatefulWidget {
 }
 
 class _MapTabState extends State<MapTab> {
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Map_Tab'),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+        },
+        scrollGesturesEnabled: true,
+      ),
     );
   }
 }
