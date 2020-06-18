90490f4bcfdc458b8b3bbee73d1183b7821efb9a
Eli <elibailey97@gmail.com>
2020-06-18 18:14:30 -0400

    updates to location service

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,30 +1,34 @@
 import 'dart:async';
-
 import 'package:geolocator/geolocator.dart';
 
 class LocationService {
 
-  // GeoLocator
+  /// GeoLocator instance
   final Geolocator _geolocator = Geolocator();
 
   void checkPermission() async {
-    await _geolocator.checkGeolocationPermissionStatus().then((status) {
-      print('status: $status');
-    });
-    await _geolocator
-        .checkGeolocationPermissionStatus(
-            locationPermission: GeolocationPermission.locationAlways)
-        .then((status) {
-      print('always status: $status');
-    });
-    await _geolocator
-        .checkGeolocationPermissionStatus(
-            locationPermission: GeolocationPermission.locationWhenInUse)
-        .then((status) {
-      print('whenInUse status: $status');
-    });
+    try {
+      await _geolocator.checkGeolocationPermissionStatus().then((status) {
+        print('status: $status');
+      });
+      await _geolocator
+          .checkGeolocationPermissionStatus(
+          locationPermission: GeolocationPermission.locationAlways)
+          .then((status) {
+        print('always status: $status');
+      });
+      await _geolocator
+          .checkGeolocationPermissionStatus(
+          locationPermission: GeolocationPermission.locationWhenInUse)
+          .then((status) {
+        print('whenInUse status: $status');
+      });
+    } catch(e) {
+      print(e.toString());
+    }
   }
 
+  /// Get Future<Position> using best accuracy
   Future<Position> getPosition() async {
     try {
       return await _geolocator.getCurrentPosition(
@@ -35,10 +39,9 @@ class LocationService {
     }
   }
 
-  Future<double> distanceFromMe(Position myPos, double lat, double long) async {
-    return _geolocator.distanceBetween(myPos.latitude, myPos.longitude, lat, long);
-  }
-
+  /// Stream changes in Position
+  ///
+  /// Default best accuracy and updates every one meter
   Stream<Position> positionStream({accuracy = LocationAccuracy.bestForNavigation, distanceFilter = 1}) {
     LocationOptions locationOptions = LocationOptions(accuracy: accuracy, distanceFilter: distanceFilter);
     return _geolocator.getPositionStream(locationOptions);
