3750365933200fe08e8edc4ff45cb71aac3e8715
Eli <elibailey97@gmail.com>
2020-06-18 18:08:26 -0400

    dart styled comments on auth service

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,33 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mvp/models/user.dart';
 import 'package:mvp/services/database.dart';
-import 'package:mvp/services/local_persistence.dart';
 
 class AuthService {
 
-  // private firebase auth instance
+  /// Firebase authentication instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final LocalPersistence _localPersistence = LocalPersistence();
   
-  // create user obj based on FirebaseUser
+  /// Create a User object from a FirebaseUser
   User _userFromFirebaseUser(FirebaseUser firebaseUser) {
     return firebaseUser != null ? User(uid: firebaseUser.uid) : null;
   }
 
-  // auth change user stream
+  /// Auth change user stream
+  ///
+  /// Streams changes in user object as authentication state changes
   Stream<User> get user {
     return _auth.onAuthStateChanged
       // Convert the Firebase user returned to a user
-      // .map((FirebaseUser user) => _userFromFirebaseUser(user));
       .map(_userFromFirebaseUser);
   }
 
-  // sign in with email and password
+  /// Sign in with email and password
+  ///
+  /// Returns User if login successful, null otherwise
   Future signInEmailPass(String email, String password) async {
     try {
+      // create a user in firebase
       AuthResult result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser firebaseUser = result.user;
+
+      // convert firebase user to User Model
       return _userFromFirebaseUser(firebaseUser);
     } catch(e) {
       print(e.toString());
@@ -36,10 +40,12 @@ class AuthService {
   }
 
 
-  // register with email and pass
+  /// Register with email and password
+  ///
+  /// Returns User if login successful, null otherwise
   Future registerEmailPass(String email, String password) async {
     try {
-      // create a user
+      // create a user in firebase
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser firebaseUser = result.user;
@@ -48,6 +54,7 @@ class AuthService {
       await DatabaseService(uid: firebaseUser.uid)
           .updateThisUserDocument(firstName: 'unnamed_member');
 
+      // convert firebase user to User Model
       return _userFromFirebaseUser(firebaseUser);
     } catch(e) {
       print(e.toString());
@@ -55,7 +62,7 @@ class AuthService {
     }
   }
 
-  // sign out
+  /// Sign out
   Future signOut() async {
     try {
       return await _auth.signOut();
