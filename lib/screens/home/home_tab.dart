275bd1d5f067286aaf2b65a244043eed350ea93f
Eli <elibailey97@gmail.com>
2020-06-22 21:42:31 -0400

    draw a background and box

--- lib/screens/home/home_tab.dart
+++ lib/screens/home/home_tab.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:mvp/models/user.dart';
-import 'package:mvp/services/database.dart';
-import 'package:mvp/services/location.dart';
-import 'package:mvp/shared/loading.dart';
-import 'package:provider/provider.dart';
 import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mvp/models/user.dart';
+import 'package:provider/provider.dart';
 import 'package:flame/game.dart';
+import 'package:flame/util.dart';
 
 class HomeTab extends StatefulWidget {
 
@@ -20,19 +18,52 @@ class _HomeTabState extends State<HomeTab> {
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
 
-    LangawGame game = LangawGame();
+    Util flameUtil = Util();
+    flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+    BoxGame game = BoxGame();
     return game.widget;
   }
 }
 
 
 
-class LangawGame extends Game {
+class BoxGame extends Game {
   Size screenSize;
 
-  void render(Canvas canvas) {}
+  void render(Canvas canvas) {
+    // draw a rectangle for the background
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    // make a paint object to fill in the rectangle
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff000000);
+    // fill in the background rectangle
+    canvas.drawRect(bgRect, bgPaint);
+
+    // draw a box
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect = Rect.fromLTWH(
+        screenCenterX - 75,
+        screenCenterY - 75,
+        150,
+        150
+    );
+    Paint boxPaint = Paint();
+    boxPaint.color = Color(0xffffffff);
+    canvas.drawRect(boxRect, boxPaint);
+
+
+
+
+
+
+  }
 
   void update(double t) {}
 
-  void resize(Size size) {}
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
+  }
 }
